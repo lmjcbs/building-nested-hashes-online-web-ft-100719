682295e97630c99d65fe35de4b0d6e7e1f57e9ae
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-15 22:39:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,9 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
+      :patriarch => {
+        
+      },
       :matriarch => {},
       :hero => {},
       :hero_friends => []
