a3b096bd6a976f98518e8744683e1906345b5f07
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-15 22:45:59 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,7 +24,7 @@ def fourth_challenge
       :heroine_friends => [
         {
           :name => "Steven",
-          :age => "30"
+          :age => "30",
           :attitude => "confused"
         }
       ]
